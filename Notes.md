b6c5e206ea74b1655b1ff32cd230617de1355bc8
Abhishek Pal <abhipal2330@gmail.com>
2025-05-29 18:24:07 +0530

    playing with http methods

---
+++ Notes.md
@@ -0,0 +1,21 @@
+
+app.use('/',(req,res)=>{
+    res.send("Namaste JI")
+})
+
+
+Here routes matching pe kaam karta hai / hai 
+uske aage kuchh bhi hoga to / walaa code hi chalega
+
+2) Order of routes is important kyuki starting se routes match karta 
+hai jab request api ke pass jata hai
+
+3) Bydefault you type url in browser is get api call 
+
+4) Dont use browser for api testing use postman 
+
+5) .use  and other api creation like get post delete mein ya farak hai
+
+6) use wala sabhi api calls ko  match karta hai and get wala sirf get ko karega  post wala sirf post ko karega
+
+
--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 const express=require('express')
 const app=express()
 
+
+app.get('/users', (req,res)=>{
+    res.send({firstname:'Abhishek', lastname:"Pal"})
+})
+
+app.post('/users',(req,res)=>{
+    console.log("Heloo post man post kar do");
+    res.send('postman ji post')
+})
+app.patch('/users',(req,res)=>{
+    console.log("Heloo post man post kar do");
+    res.send('patcht')
+})
+
 app.use('/test',(req,res)=>{
     res.send("hello ji server se")
+})
+
+app.use('/hello/2',(req,res)=>{
+    res.send("ABRACADABRA")
+})
+app.use('/hello',(req,res)=>{
+    res.send("hello")
 })
 
 
