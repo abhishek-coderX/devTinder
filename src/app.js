b5a83c83497c57863dea5fe0bfe738d605880be1
Abhishek Pal <abhipal2330@gmail.com>
2025-05-29 19:36:10 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const express=require('express')
 const app=express()
 
-app.get('/users/:name/:pass/:age',(req,res)=>{
-    console.log(req.query)
-    console.log(req.params)
-    res.send("Hii i am funny routes ")
-})
-
-
+app
 
 app.listen(4000,()=>{
     console.log("server running");
 })
+
+
