c4b1fae97c184bf635a8a0018eb8223651f150a7
Abhishek Pal <abhipal2330@gmail.com>
2025-07-11 20:33:12 +0530

    profile/password/edit

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const userAuth = require("../middlewares/Auth");
 const { validateProfileEditData } = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const profileRouter = express.Router();
 
@@ -29,6 +32,37 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
       message: `${loggedInUser.firstName}, your Profile updated successfully`,
       data: loggedInUser,
     });
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
+  }
+});
+
+profileRouter.patch("/profile/password/edit", userAuth, async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user._id).select("+password");
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch) {
+      throw new Error("Incorrect current password");
+    }
+    if (oldPassword === newPassword) {
+      throw new Error("New password must be different from old password");
+    }
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error("New password is not strong enough");
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+    });
+
+    res.send("Password updated successfully. Please login again.");
   } catch (error) {
     res.status(400).send("Error: " + error.message);
   }
