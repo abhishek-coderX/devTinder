ea52157fb282cfc0e3576cf9e7e8bec07e0a3423
Abhishek Pal <abhipal2330@gmail.com>
2025-08-19 14:46:25 +0530

    http

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,14 @@
-const express=require("express")
+const express = require("express");
 const User = require("../models/user");
-const {validateSignupData} =require("../utils/validation")
-const bcrypt=require("bcrypt")
+const { validateSignupData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const authRouter=express.Router()
-
+const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res) => {
   try {
-    
-   validateSignupData(req)
+    validateSignupData(req);
 
     const {
       firstName,
@@ -38,54 +36,60 @@ authRouter.post("/signup", async (req, res) => {
       skills,
     });
 
-   const savedUser= await user.save();
-    const token= await jwt.sign({_id:savedUser ._id},process.env.JWT_SECRET)
-      res.cookie("token",token)
+    const savedUser = await user.save();
+    const token = await jwt.sign(
+      { _id: savedUser._id },
+      process.env.JWT_SECRET
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
 
-    res.json({message:"Signup successful",data:savedUser});
+    res.json({ message: "Signup successful", data: savedUser });
   } catch (error) {
     res.status(400).send("Error saving the data: " + error.message);
   }
 });
 
-
-authRouter.post("/login",async(req,res)=>{
+authRouter.post("/login", async (req, res) => {
   try {
     //email and password nikalo
-    const {email,password}=req.body
-    
-    const user=await User.findOne({email}).select("+password")
-    if(!user)
-    {
-      throw new Error("Email or password is invalid")
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      throw new Error("Email or password is invalid");
     }
 
-    const isPasswordValid=await bcrypt.compare(password,user.password)
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(isPasswordValid)
-    {  
-      const token= await jwt.sign({_id:user._id},process.env.JWT_SECRET)
-      res.cookie("token",token)
-      res.send(user)
-    }
-    else{
-      throw new Error("Email or password is invalid")
-    }
+    if (isPasswordValid) {
+      const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
 
-    
+      res.send(user);
+    } else {
+      throw new Error("Email or password is invalid");
+    }
   } catch (error) {
     res.status(400).send("Error " + error.message);
   }
-})
+});
 
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+  res.send("Logged out successfully");
+});
 
-authRouter.post('/logout',async (req,res)=>{
-   res.cookie('token',null, {
-    expires:new Date(Date.now())
-  })
-  res.send("Logged out successfully")
-})
-
-
-
-module.exports=authRouter
+module.exports = authRouter;
